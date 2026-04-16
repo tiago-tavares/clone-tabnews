123af0e1edf70b68cdd3acef1d832354a8c649d5
tiago-tavares <80044188+tiago-tavares@users.noreply.github.com>
2026-04-16 15:56:10 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-function Home () {
-    return <h1>Ariane, eu te amo demais, e se você me ama
-        me dê um abraço e um beijo! s2
-        É sensacional o que esta acontecendo!
+function Home() {
+  return (
+    <h1>
+      Ariane, eu te amo demais, e se você me ama me dê um abraço e um beijo! s2
+      É sensacional o que esta acontecendo!
     </h1>
+  );
 }
 
 export default Home;
